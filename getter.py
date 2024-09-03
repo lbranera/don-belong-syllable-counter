e9bc3f4758bbea0659485e1cdbaf0d4533899139
Lorenz Timothy Barco Ranera <lbranera@up.edu.ph>
2024-09-03 14:48:17 +0800

    updated getter.py

--- getter.py
+++ getter.py
@@ -1,17 +1,3 @@
-import random
-
-file = open("syllables.txt")
-syllables = [line.strip() for line in file.readlines()]
-file.close()
-
-file = open("vocabulary.txt")
-vocab = [line.strip() for line in file.readlines()]
-file.close()
-
-def get_sample(n):
-    sample = random.sample(vocab, n)
-    return sample
-
 def get_doc(filename):
     file = open(filename)
     doc = [line.strip().split(" ") for line in file.readlines()]
